9d0887f24bc601dcf201b5ad1d09a039950e8c35
Graham Cox <graham@grahamcox.co.uk>
2014-06-29 11:29:02 +0100

    Actually provide an implemtation of the main entrypoints. Doesn't do anything useful yet.

---
+++ src/lib/main/main.cpp
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <iostream>
+
+/**
+ * Initialize the application
+ */
+void initialize() {
+  std::cerr << "Initializing" << std::endl;
+}
+
+/**
+ * Set the arguments that will be used by the application
+ * @param args The arguments to use
+ */
+void setArguments(const std::vector<const char*>& args) {
+  std::cerr << "Processing arguments" << std::endl;
+  for (const char* a : args) {
+    std::cerr << "Argument: " << a << std::endl;
+  }
+}
+
+/**
+ * Start running the application
+ */
+void start() {
+  std::cerr << "Starting" << std::endl;
+}
